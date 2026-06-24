e95411596107bf4b4c4756c4d36e4cece27b0b92
lalston07 <leilaalston07@gmail.com>
2026-06-24 13:24:36 -0400

    Bug Fixes

--- xicsrt_contrib/sources/_XicsrtPlasmaVmec.py
+++ xicsrt_contrib/sources/_XicsrtPlasmaVmec.py
@@ -148,7 +148,7 @@ class XicsrtPlasmaVmec(XicsrtPlasmaGeneric):
         point_check = self.car_from_flx(point_flx_temp)
         rho = np.asarray(point_flx_temp[:, 0]).copy()
         error = np.linalg.norm(point_check - points, axis=1)
-        rho[(~np.isfinite(rho)) | (eho >= 1.0) | (error > 1e-2)] = np.nan
+        rho[(~np.isfinite(rho)) | (rho >= 1.0) | (error > 1e-2)] = np.nan
         
         profiler.stop("Fluxspace from Realspace")
         
